687bea094b286e74d33a150f3594836e697bcb62
Mario Rojas <135486752+MarioDevelop3r@users.noreply.github.com>
2024-05-19 23:15:55 -0400

    chore: Update Balance component to format budget amount

--- subcripciones/src/components/Balance.jsx
+++ subcripciones/src/components/Balance.jsx
@@ -1,9 +1,11 @@
+import { moneyFormat } from "./helpers";
+
 
 
 const Balance = ({ count }) => {
     return (
         <div className="balance">
-            <h3>Presupeuesto: {count}</h3>
+            <h3>Presupeuesto: {moneyFormat(count)}</h3>
             <h3>Disponible: {count}</h3>
             <h3>Gastado: {count}</h3>
         </div>
--- subcripciones/src/components/FormAddSubs.jsx
+++ subcripciones/src/components/FormAddSubs.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
 
 
-const FormAddSubs = ({ setType, setPrice, type, price }) => {
+const FormAddSubs = ({ setType, setPrice, type, price, setSubs, subs, editId, setEditId}) => {
+    const [error, setError] = useState(false);
 
     const handleSubs = e => {
         e.preventDefault();
-        console.log(type);
-        console.log(price);
+        if (price === "" || Number(price) < 0 || type === "") {
+            setError(true);
+            return;
+        }
+        setError(false);
+        if(editId != ""){
+            setEditId("");
+            const newSubs = subs.map(item => {
+                if(item.id === editId){
+                    item.type = type;
+                    item.price = price;
+                }
+                return item;
+            })
+            setSubs(newSubs);
+        } else {
+            const data = {
+                type: type,
+                price: price,
+                id: Date.now()
+        }
+        
+        
+        setSubs([...subs, data]);
+    }
+        setType("");
+        setPrice("");
+        // console.log(subs)
     }
 
     return (
         <div className="add-subscription">
             <h3>Agregar Suscripciones</h3>
-            <form onSubmit={ handleSubs }>
+            <form onSubmit={handleSubs}>
                 <p>Servicio</p>
-                <select onChange={e => setType(e.target.value)}>
-                    <option value="">-- ELegir --</option>
+                <select onChange={e => setType(e.target.value)} value={type}>
+                    <option value="">-- Elegir --</option>
                     <option value="netflix">Netflix</option>
                     <option value="disneyPlus">Disney PLus</option>
                     <option value="hboMax">HBO Max</option>
@@ -24,8 +52,12 @@ const FormAddSubs = ({ setType, setPrice, type, price }) => {
                     <option value="appletv">Apple Tv</option>
                 </select>
                 <p>cantidad</p>
-                <input type="submit" value="Agregar"  />
+                <input type="number" placeholder="20$" onChange={e => setPrice(e.target.value)} value={price} />
+                {editId != "" ? <input type="submit" value="Guardar" />
+                :<input type="submit" value="Agregar" />}
+                
             </form>
+            {error ? <p className="error">Campos inavlidos</p> : null}
         </div>
     )
 }
